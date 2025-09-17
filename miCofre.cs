9aefcacf67dfa90e7d893dab1987cdfb833f54d3
Isabel Sánchez Berriel <isanchez@ull.local>
2025-09-17 19:00:25 +0100

    cambio tamaño

---
+++ miCofre.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miCofre : MonoBehaviour
+{
+    
+    public int tamano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tamano = 2;
+        Debug.Log(tamano);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cambiarTamano();
+    }
+
+    void cambiarTamano(){
+        tamano = 2 * tamano;
+        Debug.Log(tamano);
+    }
+}
